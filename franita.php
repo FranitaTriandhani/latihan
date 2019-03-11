93ea77177c2604a7f21fbd97201efea54fac163e
FranitaTriandhani <48430884+FranitaTriandhani@users.noreply.github.com>
2019-03-11 14:34:53 +0700

    Add files via upload

---
+++ franita.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<br>
+<br>
+
+<?php
+echo "...";
+?>
+
+<br>
+<center><h1>HELLO WORLD...!<h1></center>
+<p>Franita Triandhani</p>
+<p>16.01.53.0006</p>
+
+
+</body>
+</html>
